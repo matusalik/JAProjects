b93660b85db1624964156a9505fdb1b5b815b190
matusalik <mateuszsalik@gmail.com>
2024-10-21 22:25:39 +0200

    Threads added

--- MotionBlurFilter/MotionBlurFilter/Program.cs
+++ MotionBlurFilter/MotionBlurFilter/Program.cs
@@ -5,24 +5,38 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-
+public class ASM {
+    [DllImport(@"C:\Users\mateu\OneDrive\Pulpit\JAProjects\MotionBlurFilter\x64\Debug\MotionBlurASM.dll")]
+    static extern int MyProc1(int a, int b);
+    public void CallAsmProcedure() {
+        int x = 10, y = 15;
+        int res = MyProc1(x, y);
+        Console.Write("Wartości dodane w ASM: ");
+        Console.WriteLine(res);
+    }
+}
+public class C{
+    [DllImport(@"C:\Users\mateu\OneDrive\Pulpit\JAProjects\MotionBlurFilter\x64\Debug\MotionBlurC.dll")]
+    static extern int add(int a, int b);
+    public void CallCFunction(){
+        int x = 10, y = 15;
+        int res = add(x, y);
+        Console.Write("Wartości dodane w C: ");
+        Console.WriteLine(res);
+    }
+}
 namespace MotionBlurFilter
 {
     class Program
-    {
-        [DllImport(@"C:\Users\mateu\OneDrive\Pulpit\JAProjects\MotionBlurFilter\x64\Debug\MotionBlurASM.dll")]
-        static extern int MyProc1(int a, int b);
-        [DllImport(@"C:\Users\mateu\OneDrive\Pulpit\JAProjects\MotionBlurFilter\x64\Debug\MotionBlurC.dll")]
-        static extern int add(int a, int b);
+    {       
         static void Main(string[] args)
         {
-            int x = 5, y = 3;
-            int retValasm = MyProc1(x, y);
-            int retValC = add(x, y);
-            Console.Write("Moja pierwsza wartość obliczona w asm to:");
-            Console.WriteLine(retValasm);
-            Console.Write("Moja pierwsza wartość obliczona w C to:");
-            Console.WriteLine(retValC);
+            ASM asm = new ASM();
+            Thread threadAsm = new Thread(new ThreadStart(asm.CallAsmProcedure));
+            threadAsm.Start();
+            C c = new C();
+            Thread threadC = new Thread(new ThreadStart(c.CallCFunction));
+            threadC.Start();
         }
     }
 }
