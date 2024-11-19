9efbc554bb5901aa6aefe814b03e8e4180089ed3
matusalik <mateuszsalik@gmail.com>
2024-11-19 11:56:04 +0100

    Minor changes:

--- MotionBlurFilter/MotionBlurASM/MotionBlurASM.asm
+++ MotionBlurFilter/MotionBlurASM/MotionBlurASM.asm
@@ -1,3 +1,8 @@
+.data
+Brightness DWORD 30
+bytesPerPixel dq 3
+offsetCount dq 0 
+
 .code
 MyProc1 proc 
     ;stack pointer
@@ -17,18 +22,39 @@ MyProc1 proc
 
     ;calculating stride and loading it to r11
     mov r11, rcx
-    mov r12, 3
-    imul r11, r12
+    imul r11, [bytesPerPixel]
 
     ;calculating move index for xmm
     mov r12, rax
-    mov r13, 3
-    imul r12, r13
+    imul r12, [bytesPerPixel]
+
+    movd xmm1, Brightness
+    pshufd xmm1, xmm1, 0
+
+    ;Calculating total bytes
+    mov r14, rcx
+    imul r14, rdx
+    imul r14, [bytesPerPixel]
+
+    ;Offset for copying pixels
+    mov r13, 0
 
 ProcessCollumns:
-    cmp rax, rbx
-    jge EndFunc
+    cmp r12, rbx
+    jge CopyPixels
+    movups xmm0, [rsi + r12]
+    paddusb xmm0, xmm1
+    movups [rdi + r12], xmm0
+    add r12, 12
+    jmp ProcessCollumns
 
+CopyPixels:
+    cmp r13, r14
+    jge EndFunc
+    mov al, [rdi + r13]
+    mov[rsi + r13], al
+    inc r13
+    jmp CopyPixels
 
 EndFunc:
     mov rsp, rbp 
