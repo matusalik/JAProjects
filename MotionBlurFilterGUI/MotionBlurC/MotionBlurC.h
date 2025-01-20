3502015346f18e4644ea59f01d5902a6ed8d2395
matusalik <mateuszsalik@gmail.com>
2025-01-20 22:54:34 +0100

    Rollback

--- MotionBlurFilterGUI/MotionBlurC/MotionBlurC.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#ifdef MotionBlurC_EXPORTS
-#define MotionBlurC_API __declspec(dllexport)
-#else
-#define MotionBlurC_API __declspec(dllimport)
-#endif
-
-extern "C" MotionBlurC_API void ApplyMotionBlur(uint8_t* ptr, uint8_t* temp, int startX, int endX, int width, int height, int radius);
